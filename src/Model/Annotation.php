a621551c83d5edbd6d39f6f7ef11517ebcd352a0
scottmelville <scott.melville@digirati.com>
2017-06-27 10:35:45 +0100

    NLW-274 add accessors to Annotation model to allow modification by other modules

--- src/Model/Annotation.php
+++ src/Model/Annotation.php
@@ -4,44 +4,112 @@ namespace Elucidate\Model;
 
 use ArrayAccess as ArrayAccessInterface;
 
-class Annotation implements RequestModel, ResponseModel, ArrayAccessInterface
-{
-    use JsonLDContext;
-    use SerializeToJsonLD;
-    use ArrayAccess;
+class Annotation implements RequestModel, ResponseModel, ArrayAccessInterface {
+  use JsonLDContext;
+  use SerializeToJsonLD;
+  use ArrayAccess;
 
-    private $type;
-    private $body;
-    private $target;
-    private $id;
-    private $creator;
-    private $generator;
-    private $container;
+  private $type;
+  private $body;
+  private $target;
+  private $id;
+  private $creator;
+  private $generator;
+  private $container;
 
-    public function getContainer()
-    {
-        return $this->container;
-    }
+  public function getContainer() {
+    return $this->container;
+  }
 
-    public function __construct(
-        string $id = null,
-        $body = null,
-        $target = null,
-        $creator = null,
-        $generator = null,
-        Container $container = null
-    ) {
-        $this->type = 'Annotation';
-        $this->body = $body;
-        $this->target = $target;
-        $this->id = $id;
-        $this->creator = $creator;
-        $this->generator = $generator;
-        $this->container = $container;
-    }
+  public function __construct(
+    string $id = NULL,
+    $body = NULL,
+    $target = NULL,
+    $creator = NULL,
+    $generator = NULL,
+    Container $container = NULL
+  ) {
+    $this->type = 'Annotation';
+    $this->body = $body;
+    $this->target = $target;
+    $this->id = $id;
+    $this->creator = $creator;
+    $this->generator = $generator;
+    $this->container = $container;
+  }
 
-    public function __toString()
-    {
-        return substr($this->id, -1) === '/' ? $this->id : $this->id.'/';
-    }
+  /**
+   * @return null
+   */
+  public function getBody() {
+    return $this->body;
+  }
+
+  /**
+   * @param null $body
+   */
+  public function setBody($body) {
+    $this->body = $body;
+  }
+
+  /**
+   * @return null
+   */
+  public function getTarget() {
+    return $this->target;
+  }
+
+  /**
+   * @param null $target
+   */
+  public function setTarget($target) {
+    $this->target = $target;
+  }
+
+  /**
+   * @return string
+   */
+  public function getId(): string {
+    return $this->id;
+  }
+
+  /**
+   * @param string $id
+   */
+  public function setId(string $id) {
+    $this->id = $id;
+  }
+
+  /**
+   * @return null
+   */
+  public function getCreator() {
+    return $this->creator;
+  }
+
+  /**
+   * @param null $creator
+   */
+  public function setCreator($creator) {
+    $this->creator = $creator;
+  }
+
+  /**
+   * @return null
+   */
+  public function getGenerator() {
+    return $this->generator;
+  }
+
+  /**
+   * @param null $generator
+   */
+  public function setGenerator($generator) {
+    $this->generator = $generator;
+  }
+
+
+  public function __toString() {
+    return substr($this->id, -1) === '/' ? $this->id : $this->id . '/';
+  }
 }
